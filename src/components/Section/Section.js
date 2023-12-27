7669ad0855e82b9a7b0c585d755bf25a453b629b
Prodipta Bose <nielbose1996@gmail.com>
2023-12-27 17:11:47 +0530

    finished

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,31 +1,61 @@
 import Card from "../Card/Card";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Section.module.css"
 import { CircularProgress } from "@mui/material";
 import Carousel from "../Carousel/Carousel";
-export default function Section({title,data,type}){
+import Filters from "../Filters/Filters";
+
+export default function Section({title,data,filterSource,type}){
     const[carouselToggle,setCarouselToggle]=useState(true);
+    const [filters,setFilters] = useState([{key:"all",label:"All"}]);
+    const [selectedFilterIndex,setSelectedFilterndex]=useState(0);
+
     const handleToggle = () =>{
-     setCarouselToggle(   (prevState)=> !prevState);
+     setCarouselToggle((prevState)=> !prevState);
     }
+    
+    useEffect(()=>{
+        if(filterSource){
+           filterSource().then((response)=>{
+             const {data}=response;
+             setFilters([...filters,...data]);
+           });
+        }
+    },[]);
+
+    const showFilters=filters.length>1;
+    const cardsToRender = data.filter((card)=>
+       showFilters && selectedFilterIndex!==0
+       ? card.genre.key === filters[selectedFilterIndex].key
+       :card )
+
+
     return(
         <div>
             <div className={styles.header}>
                 <h3>{title}</h3>
                 <h4 className={styles.toggleText} onClick={handleToggle}>{!carouselToggle ? "Collapse All" : "Show All "}</h4>
             </div>
+            {showFilters && ( <div className={styles.filterWrapper}> 
+                <Filters
+                filters={filters}
+                selectedFilterIndex={selectedFilterIndex}
+                setSelectedFilterIndex={setSelectedFilterndex}
+                />
+            </div>
+            )}
             {data.length === 0 ? (
              <CircularProgress/>
             ) :(
                 <div className={styles.cardWrapper}>
                    {!carouselToggle ? (
                    <div className={styles.wrapper}>
-                    {data.map((ele)=>(
+                    {cardsToRender.map((ele)=>(
                       <Card data={ele} type={type}/>
                     ))}
                    </div>) 
                    : (
-                       <Carousel data={data}
+                       <Carousel data={cardsToRender}
                          renderComponent={(data)=><Card data={data} type={type}/>}
                        />
                    )}
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,6 +3,7 @@ import styles from "./HomePage.module.css";
 import { useOutletContext } from "react-router-dom";
 import Hero from "../../components/Hero/Hero";
 import Section from "../../components/Section/Section"
+import { fetchFilters } from "../../api/api";
 
 function HomePage(){
 const {data}=useOutletContext();
@@ -14,7 +15,7 @@ return(
     <div className={styles.wrapper}>
         <Section title="Top Albums" data={topAlbums} type="album"/>
         <Section title="New Albums" data={newAlbums} type="album"/>
-        {/*<Section title="Songs" data={songs} type="song"/>*/}
+        <Section title="Songs" data={songs} filterSource={fetchFilters} type="song"/>
 
     </div>
     </>
